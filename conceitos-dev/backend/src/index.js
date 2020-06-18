4e6743e45332775a851fbdcd9fe55010455cacdc
Gustavo Tenório <gustavohst@msn.com>
2020-06-17 21:02:33 -0300

    Create rotes GET PUT UPDATE and DELETE

--- conceitos-dev/backend/src/index.js
+++ conceitos-dev/backend/src/index.js
@@ -1,12 +1,65 @@
 const express = require('express');
+const { uuid } = require('uuidv4')
 
 const app = express();
 
+app.use(express.json());
+
+const projects = [];
 //ROTE
-app.get('/', (request, response) => {
-   return response.json({
-      message: 'Hello go Gustavo'
-   });
+app.get('/projects', (request, response) => {
+   const { title } = request.query;
+
+   const results = title
+      ? projects.filter(project => project.title.includes(title))
+      : projects;
+
+   return response.json(results);
+}); 
+
+app.post('/projects', (request, response) => {
+   const { title, owner } = request.body;
+
+   const project = { id: uuid(), title, owner };
+
+   projects.push(project);
+
+   return response.json(project);
+}); 
+
+app.put('/projects/:id', (request, response) => {
+   const { id } = request.params;
+   const { title, owner } = request.body;
+
+   const projectIndex = projects.findIndex(project => project.id === id);
+
+   if(projectIndex < 0){
+      return response.status(400).json( { error: 'Project not found' } )
+   } 
+
+   const project = {
+      id,
+      title,
+      owner,
+   };
+
+   projects[projectIndex] = project;
+
+   return response.json(project);
+}); 
+
+app.delete('/projects/:id', (request, response) => {
+   const { id } = request.params;
+
+   const projectIndex = projects.findIndex(project => project.id === id);
+
+   if(projectIndex < 0){
+      return response.status(400).json( { error: 'Project not found' } )
+   } 
+
+   projects.splice(projectIndex, 1);
+
+   return response.status(204).send();
 }); 
 
 app.listen(3333, () => {
