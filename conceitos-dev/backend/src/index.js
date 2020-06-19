c5532c6a84046e5311aad8f67ab3c2afdb31d799
Gustavo Tenório <gustavohst@msn.com>
2020-06-19 13:06:13 -0300

    Add log requests and Validation project ID

--- conceitos-dev/backend/src/index.js
+++ conceitos-dev/backend/src/index.js
@@ -1,11 +1,39 @@
 const express = require('express');
-const { uuid } = require('uuidv4')
+const { uuid, isUuid } = require('uuidv4')
 
 const app = express();
 
 app.use(express.json());
 
 const projects = [];
+
+function logRequests(request, response, next){
+   const { method, url}  = request
+
+   const logLabel = `[${method.toUpperCase()}] ${url}`;
+
+   console.log(logLabel);
+
+   console.time(logLabel);
+
+   next();
+
+   console.timeEnd(logLabel);
+}
+
+function validateProjectId( request, response, next){
+   const { id } = request.params;
+
+   if(!isUuid(id)){
+      return response.status(400).json({error: 'invalid project ID.' });
+   }
+
+   return next();
+}
+
+app.use(logRequests);
+app.use('/projects/:id', validateProjectId)
+
 //ROTE
 app.get('/projects', (request, response) => {
    const { title } = request.query;
